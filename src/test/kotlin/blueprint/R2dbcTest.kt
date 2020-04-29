fb0f053dd60aa6254466c345baf215022c7572c7
Christoph Sturm <me@christophsturm.com>
2020-04-29 21:07:43 +0200

    insert into table and get back autoincrement value

--- src/test/kotlin/blueprint/R2dbcTest.kt
+++ src/test/kotlin/blueprint/R2dbcTest.kt
@@ -4,6 +4,7 @@ import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
 import io.r2dbc.spi.Connection
 import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.test.runBlockingTest
@@ -13,7 +14,7 @@ import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
 
-@Suppress("SqlNoDataSourceInspection")
+@Suppress("SqlNoDataSourceInspection", "SqlResolve")
 @ExperimentalCoroutinesApi
 class R2dbcTest : JUnit5Minutests {
 
@@ -27,12 +28,22 @@ class R2dbcTest : JUnit5Minutests {
                 ConnectionFactories.get("r2dbc:h2:mem:///r2dbc-test;DB_CLOSE_DELAY=-1")
             runBlockingTest {
                 val connection: Connection = connectionFactory.create().awaitSingle()
-                val result = connection.createStatement("select 42").execute().awaitSingle()
-                val field = result.map { a, _ ->
-                    a.get(0, Integer::class.java)
-                }.awaitSingle()
-                expectThat(field.toInt()).isEqualTo(42)
+                val firstInsertResult =
+                    connection.createStatement("insert into USERS values(NULL, $1)").bind("$1", "user42")
+                        .returnGeneratedValues().execute().awaitSingle()
+                val secondInsertResult =
+                    connection.createStatement("insert into USERS values(NULL, $1)").bind("$1", "user42")
+                        .returnGeneratedValues().execute().awaitSingle()
+
+                expectThat(getIntResult(firstInsertResult).toInt()).isEqualTo(1)
+                expectThat(getIntResult(secondInsertResult).toInt()).isEqualTo(2)
             }
         }
+    }
+
+    private suspend fun getIntResult(result: Result): Integer {
+        return result.map { a, _ ->
+            a.get(0, Integer::class.java)
+        }.awaitSingle()
     }
 }
---
+++ src/test/resources/db/migration/V1__create_test_tables.sql
@@ -0,0 +1,5 @@
+create table USERS
+(
+    ID            int          not null auto_increment primary key,
+    LOGIN varchar(100) not null
+);
