29b05ae9d3399ea807138a8f93cf61b1f836d807
Christoph Sturm <me@christophsturm.com>
2020-04-30 17:25:32 +0200

    run tests parallel

--- src/test/kotlin/r2dbcfun/TestUtil.kt
+++ src/test/kotlin/r2dbcfun/TestUtil.kt
@@ -4,12 +4,13 @@ import io.r2dbc.spi.ConnectionFactories
 import io.r2dbc.spi.ConnectionFactory
 import org.flywaydb.core.Flyway
 import org.h2.jdbcx.JdbcDataSource
+import java.util.*
 
 fun prepareDB(): ConnectionFactory {
     val dataSource = JdbcDataSource()
-    dataSource.setURL("jdbc:h2:mem:r2dbc-test;DB_CLOSE_DELAY=-1")
+    val uuid = UUID.randomUUID()
+    dataSource.setURL("jdbc:h2:mem:r2dbc-test$uuid;DB_CLOSE_DELAY=-1")
     val flyway = Flyway.configure().dataSource(dataSource).load()
-    flyway.clean()
     flyway.migrate()
-    return ConnectionFactories.get("r2dbc:h2:mem:///r2dbc-test;DB_CLOSE_DELAY=-1")
+    return ConnectionFactories.get("r2dbc:h2:mem:///r2dbc-test$uuid;DB_CLOSE_DELAY=-1")
 }
