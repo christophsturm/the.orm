8ec282b71fbebc0927677060263dcb4075db9b2e
Christoph Sturm <me@christophsturm.com>
2020-04-30 16:22:09 +0200

    introduce R2dbcRepo

--- src/test/kotlin/r2dbcfun/R2dbcRepoTest.kt
+++ src/test/kotlin/r2dbcfun/R2dbcRepoTest.kt
@@ -2,10 +2,9 @@ package r2dbcfun
 
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
-import io.r2dbc.spi.Connection
-import io.r2dbc.spi.ConnectionFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -17,15 +16,15 @@ data class User(val id: Int? = null, val name: String, val email: String?)
 @ExperimentalCoroutinesApi
 class R2dbcRepoTest : JUnit5Minutests {
 
-    fun tests() = rootContext<ConnectionFactory> {
+    fun tests() = rootContext<R2dbcRepo<User>> {
         fixture {
-            prepareDB()
+            runBlocking {
+                R2dbcRepo.create<User>(prepareDB().create().awaitSingle())
+            }
         }
         test("can insert data class") {
             runBlockingTest {
-                val connection: Connection = fixture.create().awaitSingle()
-                val instance = User(name = "chris", email = "my email")
-                val user = connection.create(User(name = "chris", email = "my email"), instance::class)
+                val user = fixture.create(User(name = "chris", email = "my email"))
                 expectThat(user).and {
                     get { id }.isNotNull().isGreaterThan(0)
                     get { name }.isEqualTo("chris")
