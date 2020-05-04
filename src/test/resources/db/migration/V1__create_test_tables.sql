e216e91a9c6768327603c858dfcf0994f7640087
Christoph Sturm <me@christophsturm.com>
2020-05-04 16:36:32 +0200

    use all lowercase in ddl

--- src/test/resources/db/migration/V1__create_test_tables.sql
+++ src/test/resources/db/migration/V1__create_test_tables.sql
@@ -1,11 +1,11 @@
 create sequence users_id_seq no maxvalue;
 create table users
 (
-    ID     bigint       not null default nextval('users_id_seq'),
-    NAME   varchar(100) not null,
-    EMAIL  varchar(100),
-    ISCOOL boolean
+    id     bigint       not null default nextval('users_id_seq'),
+    name   varchar(100) not null,
+    email  varchar(100),
+    iscool boolean
 );
 
 alter table users
-    add primary key (ID);
+    add primary key (id);
