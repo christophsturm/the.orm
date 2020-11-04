07d866e18e72da0bfc756d2f5801a599a7099d13
Christoph Sturm <me@christophsturm.com>
2020-11-04 13:26:20 +0100

    test query factory property

--- src/test/kotlin/r2dbcfun/RepositoryTest.kt
+++ src/test/kotlin/r2dbcfun/RepositoryTest.kt
@@ -1,42 +1,48 @@
 package r2dbcfun
 
 import io.kotest.core.spec.style.FunSpec
+import r2dbcfun.query.QueryFactory
+import r2dbcfun.test.TestObjects.Entity
 import strikt.api.expectCatching
+import strikt.api.expectThat
 import strikt.assertions.contains
 import strikt.assertions.isA
 import strikt.assertions.isFailure
 import strikt.assertions.isNotNull
 import strikt.assertions.message
 
-class RepositoryTest :
-    FunSpec(
-        {
-            context("fail fast error handling") {
-                test("fails fast if PK has more than one field") {
-                    data class MismatchPK(override val id: Long, val blah: String) : PK
-                    data class Mismatch(val id: MismatchPK)
-                    expectCatching { Repository.create<Mismatch>() }.isFailure()
-                        .isA<RepositoryException>()
-                        .message
-                        .isNotNull()
-                        .contains("PK classes must have a single field of type long")
-                }
-                test("fails if class contains unsupported fields") {
-                    data class Unsupported(val field: String)
-                    data class ClassWithUnsupportedType(val id: Long, val unsupported: Unsupported)
-                    expectCatching { Repository.create<ClassWithUnsupportedType>() }.isFailure()
-                        .isA<RepositoryException>()
-                        .message
-                        .isNotNull()
-                        .contains("type Unsupported not supported")
-                }
-                test("fails if class has no id field") {
-                    expectCatching { Repository.create<Any>() }.isFailure()
-                        .isA<RepositoryException>()
-                        .message
-                        .isNotNull()
-                        .contains("class Any has no field named id")
-                }
+class RepositoryTest : FunSpec({
+    context("the repository class") {
+        test("returns a query factory") {
+            val queryFactory = Repository.create<Entity>().queryFactory
+            expectThat(queryFactory).isA<QueryFactory<Entity>>()
+        }
+        context("fail fast error handling") {
+            test("fails fast if PK has more than one field") {
+                data class MismatchPK(override val id: Long, val blah: String) : PK
+                data class Mismatch(val id: MismatchPK)
+                expectCatching { Repository.create<Mismatch>() }.isFailure()
+                    .isA<RepositoryException>()
+                    .message
+                    .isNotNull()
+                    .contains("PK classes must have a single field of type long")
+            }
+            test("fails if class contains unsupported fields") {
+                data class Unsupported(val field: String)
+                data class ClassWithUnsupportedType(val id: Long, val unsupported: Unsupported)
+                expectCatching { Repository.create<ClassWithUnsupportedType>() }.isFailure()
+                    .isA<RepositoryException>()
+                    .message
+                    .isNotNull()
+                    .contains("type Unsupported not supported")
+            }
+            test("fails if class has no id field") {
+                expectCatching { Repository.create<Any>() }.isFailure()
+                    .isA<RepositoryException>()
+                    .message
+                    .isNotNull()
+                    .contains("class Any has no field named id")
             }
         }
-    )
+    }
+})
