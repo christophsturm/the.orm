cba668d4a487b6c947740a5dae5426d09e318d83
Christoph Sturm <me@christophsturm.com>
2020-11-11 13:02:55 +0100

    move functional test entities to separate file

--- src/test/kotlin/r2dbcfun/test/functional/RepositoryFunctionalTest.kt
+++ src/test/kotlin/r2dbcfun/test/functional/RepositoryFunctionalTest.kt
@@ -29,23 +29,6 @@ import java.time.LocalDate
 private val characters = ('A'..'Z').toList() + (('a'..'z').toList()).plus(' ')
 private val reallyLongString = (1..20000).map { characters.random() }.joinToString("")
 
-data class UserPK(override val id: Long) : PK
-enum class Color {
-    RED,
-
-    @Suppress("unused")
-    BLUE
-}
-
-data class User(
-    val id: UserPK? = null,
-    val name: String,
-    val email: String?,
-    val isCool: Boolean? = false,
-    val bio: String? = null,
-    val favoriteColor: Color? = null,
-    val birthday: LocalDate? = null
-)
 
 class RepositoryFunctionalTest : FunSpec({
     forAllDatabases(this) { connection ->
---
+++ src/test/kotlin/r2dbcfun/test/functional/User.kt
@@ -0,0 +1,22 @@
+package r2dbcfun.test.functional
+
+import r2dbcfun.PK
+import java.time.LocalDate
+
+data class UserPK(override val id: Long) : PK
+enum class Color {
+    RED,
+
+    @Suppress("unused")
+    BLUE
+}
+
+data class User(
+    val id: UserPK? = null,
+    val name: String,
+    val email: String?,
+    val isCool: Boolean? = false,
+    val bio: String? = null,
+    val favoriteColor: Color? = null,
+    val birthday: LocalDate? = null
+)
