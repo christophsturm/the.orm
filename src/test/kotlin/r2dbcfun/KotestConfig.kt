e23a5aa8cc5b36ad121efab740ba44ef29520f5b
Christoph Sturm <me@christophsturm.com>
2020-11-12 16:51:20 +0100

    add test timeout

--- src/test/kotlin/r2dbcfun/KotestConfig.kt
+++ src/test/kotlin/r2dbcfun/KotestConfig.kt
@@ -3,12 +3,16 @@ package r2dbcfun
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
 import reactor.blockhound.BlockHound
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
+@ExperimentalTime
 @Suppress("unused")
 object KotestConfig : AbstractProjectConfig() {
     override val parallelism = Runtime.getRuntime().availableProcessors()
     override val isolationMode = IsolationMode.InstancePerTest
 
+    override val timeout = 10.seconds
     override fun beforeAll() {
         BlockHound.install()
     }
