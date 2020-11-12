72f5ca550653cecf1b92c2347625ae27d685b59c
Christoph Sturm <me@christophsturm.com>
2020-11-12 16:51:35 +0100

    fix hanging test

--- src/test/kotlin/r2dbcfun/query/QueryFactoryTest.kt
+++ src/test/kotlin/r2dbcfun/query/QueryFactoryTest.kt
@@ -2,7 +2,6 @@ package r2dbcfun.query
 
 import io.kotest.core.spec.style.FunSpec
 import io.mockk.MockKMatcherScope
-import io.mockk.coVerify
 import io.mockk.mockk
 import io.r2dbc.spi.Connection
 import r2dbcfun.ResultMapper
@@ -16,19 +15,19 @@ class QueryFactoryTest : FunSpec({
     context("typesafe query factory") {
         val finder = mockk<ResultMapper<Entity>>(relaxed = true)
         val queryFactory = QueryFactory(Entity::class, finder)
-        val connection = mockk<Connection>(relaxed = true)
+        val connection = mockk<Connection>()
         val condition = Entity::id.isEqualTo()
         test("can create query with one parameter") {
             val query = queryFactory.createQuery(condition)
-            query.with(connection, 1).find()
+            query.with(connection, 1)
         }
         test("can create query with two parameter") {
             val query = queryFactory.createQuery(condition, condition)
-            query.with(connection, 1, 1).find()
+            query.with(connection, 1, 1)
         }
         test("can create query with three parameter") {
             val query = queryFactory.createQuery(condition, condition, condition)
-            query.with(connection, 1, 1, 1).find()
+            query.with(connection, 1, 1, 1)
         }
     }
 
