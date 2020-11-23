c459665ba65ad3583f79ae06dbcb579a1964cda3
christoph <me@christophsturm.com>
2020-11-23 10:43:27 +0100

    use call chains

--- src/test/kotlin/r2dbcfun/transaction/TransactionTest.kt
+++ src/test/kotlin/r2dbcfun/transaction/TransactionTest.kt
@@ -6,20 +6,15 @@ import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.r2dbc.spi.Connection
 import kotlinx.coroutines.reactive.awaitFirstOrNull
-import org.reactivestreams.Publisher
 import strikt.api.expectThat
 import strikt.assertions.isTrue
 
 class TransactionTest : FunSpec({
     context("transaction support") {
-        val publisher: Publisher<Void> = mockk()
         mockkStatic("kotlinx.coroutines.reactive.AwaitKt")
-        coEvery {
-            publisher.awaitFirstOrNull()
-        }.returns(null)
         val connection = mockk<Connection>(relaxed = true)
-        coEvery { connection.beginTransaction() }.returns(publisher)
-        coEvery { connection.commitTransaction() }.returns(publisher)
+        coEvery { connection.beginTransaction().awaitFirstOrNull() }.returns(null)
+        coEvery { connection.commitTransaction().awaitFirstOrNull() }.returns(null)
         test("calls block") {
             var called = false
             connection.transaction {
