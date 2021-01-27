1d8f653e70c2a73690b14ecbf146c1259a03753d
Christoph Sturm <me@christophsturm.com>
2021-01-27 20:50:27 +0100

    simplify Transaction FT, return block result

--- src/main/kotlin/r2dbcfun/transaction/Transaction.kt
+++ src/main/kotlin/r2dbcfun/transaction/Transaction.kt
@@ -3,13 +3,14 @@ package r2dbcfun.transaction
 import io.r2dbc.spi.Connection
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 
-public suspend fun Connection.transaction(function: suspend () -> Unit) {
+public suspend fun <T> Connection.transaction(function: suspend () -> T): T {
     this.beginTransaction().awaitFirstOrNull() // also disables auto-commit
-    try {
+    val result = try {
         function()
     } catch (e: Exception) {
         this.rollbackTransaction().awaitFirstOrNull()
         throw e
     }
     this.commitTransaction().awaitFirstOrNull()
+    return result
 }
