b8e9d9ff8fd646ee93ed987f808da99a9d2741f5
Christoph Sturm <me@christophsturm.com>
2021-02-27 22:45:01 +0100

    work towards isolating r2dbc api

--- src/main/kotlin/r2dbcfun/r2dbc/ConnectionProvider.kt
+++ src/main/kotlin/r2dbcfun/r2dbc/ConnectionProvider.kt
@@ -3,10 +3,12 @@ package r2dbcfun.r2dbc
 import io.r2dbc.spi.Clob
 import io.r2dbc.spi.R2dbcException
 import io.r2dbc.spi.Row
+import io.r2dbc.spi.Statement
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitSingle
+import org.reactivestreams.Publisher
 import r2dbcfun.RepositoryException
 import r2dbcfun.transaction.transaction
 
@@ -36,12 +38,32 @@ class R2dbcConnection(val connection: io.r2dbc.spi.Connection) : io.r2dbc.spi.Co
         )
     }
 
+    override fun beginTransaction(): Publisher<Void> {
+        return connection.beginTransaction()
+    }
+
+    override fun commitTransaction(): Publisher<Void> {
+        return connection.commitTransaction()
+    }
+
+    override fun createStatement(sql: String): Statement {
+        return connection.createStatement(sql)
+    }
+
+    override fun rollbackTransaction(): Publisher<Void> {
+        return connection.rollbackTransaction()
+    }
 }
 
-class R2dbcResult(private val result: io.r2dbc.spi.Result) {
-    suspend fun rowsUpdated(): Int = result.rowsUpdated.awaitSingle()
+interface DBResult {
+    suspend fun rowsUpdated(): Int
+    fun <T : Any> map(mappingFunction: (t: R2dbcRow) -> T): Flow<T>
+}
 
-    fun <T : Any> map(mappingFunction: (t: R2dbcRow) -> T): Flow<T> {
+class R2dbcResult(private val result: io.r2dbc.spi.Result) : DBResult {
+    override suspend fun rowsUpdated(): Int = result.rowsUpdated.awaitSingle()
+
+    override fun <T : Any> map(mappingFunction: (t: R2dbcRow) -> T): Flow<T> {
         return result.map { row, _ -> mappingFunction(R2dbcRow(row)) }.asFlow()
     }
 }
