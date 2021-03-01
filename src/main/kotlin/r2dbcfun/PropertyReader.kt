d8ce51ab0663bae2e0dc460e771bb477c6d1b496
Christoph Sturm <me@christophsturm.com>
2021-03-01 18:22:55 +0100

    remove unused method

--- src/main/kotlin/r2dbcfun/PropertyReader.kt
+++ src/main/kotlin/r2dbcfun/PropertyReader.kt
@@ -1,6 +1,5 @@
 package r2dbcfun
 
-import r2dbcfun.dbio.Statement
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.isSubclassOf
@@ -11,26 +10,8 @@ internal class PropertyReader<T>(private val property: KProperty1<T, *>) {
 
     private val isEnum = kClass.isSubclassOf(Enum::class)
 
-    val dbClass = if (isEnum) String::class.java else kClass.java
     // enums are strings from the dbs view
-
-    /** bind this property's value to a Statement */
-    internal fun bindValue(statement: Statement, index: Int, instance: T): Statement {
-        val value = value(instance)
-        return try {
-            if (value == null) {
-                statement.bindNull(index, dbClass)
-            } else {
-                statement.bind(index, value)
-            }
-        } catch (e: java.lang.IllegalArgumentException) {
-            throw RepositoryException(
-                "error binding value $value to field $name with index $index",
-                e
-            )
-        }
-    }
-
+    val dbClass = if (isEnum) String::class.java else kClass.java
     fun value(instance: T): Any? {
         val value = property.call(instance)
         return if (value != null && isEnum) value.toString() else value
