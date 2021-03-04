aa0668f2464e9e84565af70ba9cc83390f9d916d
Christoph Sturm <me@christophsturm.com>
2021-03-04 14:16:40 +0100

    extract conditions

--- src/main/kotlin/r2dbcfun/query/QueryFactory.kt
+++ src/main/kotlin/r2dbcfun/query/QueryFactory.kt
@@ -9,23 +9,9 @@ import r2dbcfun.dbio.executeSelect
 import r2dbcfun.internal.IDHandler
 import r2dbcfun.util.toIndexedPlaceholders
 import r2dbcfun.util.toSnakeCase
-import java.time.LocalDate
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
-
-fun <T : Any, V> KProperty1<T, V>.isNull(): QueryFactory.Condition<Unit> =
-    QueryFactory.isNullCondition(this)
-
-fun <T : Any> KProperty1<T, String?>.like(): QueryFactory.Condition<String> =
-    QueryFactory.likeCondition(this)
-
-fun <T : Any, V : Any> KProperty1<T, V?>.isEqualTo(): QueryFactory.Condition<V> =
-    QueryFactory.isEqualToCondition(this)
-
-fun <T : Any> KProperty1<T, LocalDate?>.between():
-        QueryFactory.Condition<Pair<LocalDate, LocalDate>> =
-    QueryFactory.Condition("between ? and ?", this)
 
 class QueryFactory<T : Any> internal constructor(
     kClass: KClass<T>,
