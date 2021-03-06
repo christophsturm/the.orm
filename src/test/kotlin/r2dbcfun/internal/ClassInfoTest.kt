b89ccc000cf0667d90292a812f7ddafd64067a3f
Christoph Sturm <me@christophsturm.com>
2021-03-06 21:25:12 +0100

    classInfo knows name

--- src/test/kotlin/r2dbcfun/internal/ClassInfoTest.kt
+++ src/test/kotlin/r2dbcfun/internal/ClassInfoTest.kt
@@ -1,13 +1,16 @@
 package r2dbcfun.internal
 
 import failfast.describe
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 object ClassInfoTest {
     val context = describe(ClassInfo::class) {
-        it("collects class info for entities") {
-            data class Entity(val name: String, val id: Long? = null)
+        data class Entity(val name: String, val id: Long? = null)
 
-            ClassInfo(Entity::class, IDHandler(Entity::class), setOf())
+        val classInfo = ClassInfo(Entity::class, IDHandler(Entity::class), setOf())
+        it("knows the class name") {
+            expectThat(classInfo.name).isEqualTo("Entity")
         }
 
     }
