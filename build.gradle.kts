335d4b0514d7f3a7efc2ebfde9882c71a6f507a9
Christoph Sturm <me@christophsturm.com>
2021-04-04 22:16:31 +0200

    add root project file

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.38.0"
+    kotlin("jvm") version "1.4.32" apply false
+    kotlin("plugin.serialization") version "1.4.32" apply false
+    id("info.solidsoft.pitest") version "1.6.0" apply false
+}
+
+
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    val filtered =
+        listOf("alpha", "beta", "rc", "cr", "m", "preview", "dev", "eap")
+            .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*.*") }
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (filtered.any { it.matches(candidate.version) }) {
+                    reject("Release candidate")
+                }
+            }
+        }
+        // optional parameters
+        checkForGradleUpdate = true
+        outputFormatter = "json"
+        outputDir = "build/dependencyUpdates"
+        reportfileName = "report"
+    }
+}
+
+tasks.wrapper { distributionType = Wrapper.DistributionType.ALL }
