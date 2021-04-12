c657e6b91b52dde3f3e8d1abe755afc9e690afec
Christoph Sturm <me@christophsturm.com>
2021-04-12 12:57:14 +0200

    cleanup build deps

--- the.orm.testutil/build.gradle.kts
+++ the.orm.testutil/build.gradle.kts
@@ -1,25 +1,20 @@
 @file:Suppress("ConstantConditionIf")
 
 import io.the.orm.BuildConfig
-import io.the.orm.BuildConfig.blockHoundVersion
 import io.the.orm.BuildConfig.failfastVersion
-import io.the.orm.BuildConfig.striktVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "io.the.orm"
 
 val coroutinesVersion = "1.4.3"
 val kotlinVersion = BuildConfig.kotlinVersion
-val serializationVersion = "1.1.0"
 val testcontainersVersion = "1.15.2"
-val log4j2Version = "2.14.1"
 val vertxVersion = "4.0.3"
 val nettyVersion = "4.1.63.Final"
 
 plugins {
     java
     kotlin("jvm")
-    kotlin("plugin.serialization")
 }
 
 
@@ -33,42 +28,18 @@ dependencies {
     implementation(project(":the.orm"))
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-
-
     implementation("io.r2dbc:r2dbc-pool:0.8.6.RELEASE")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutinesVersion")
 
     implementation("io.vertx:vertx-rx-java2:$vertxVersion")
 
     implementation("io.vertx:vertx-pg-client:$vertxVersion")
-    runtimeOnly("io.netty:netty-resolver-dns-native-macos:$nettyVersion:osx-x86_64")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$coroutinesVersion")
-
-
-    testImplementation("io.strikt:strikt-core:$striktVersion")
     implementation("com.christophsturm.failfast:failfast:$failfastVersion")
 
-
-    testImplementation("com.christophsturm:randolf:0.2.0")
-//    testRuntimeOnly("io.projectreactor.netty:reactor-netty:0.9.14.RELEASE") // bump postgresql dependency
-
     implementation("org.testcontainers:postgresql:$testcontainersVersion")
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutinesVersion")
-    testImplementation("io.projectreactor.tools:$blockHoundVersion")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
-
-    testImplementation("org.apache.logging.log4j:log4j-core:$log4j2Version")
-    testImplementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
-    testImplementation("org.apache.logging.log4j:log4j-jul:$log4j2Version")
-    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
-
-    testImplementation("org.junit.platform:junit-platform-launcher:1.7.1")
 }
 configure<JavaPluginConvention> { sourceCompatibility = JavaVersion.VERSION_1_8 }
-//kotlin { explicitApi() }
-val needsRedefinition = JavaVersion.current().ordinal >= JavaVersion.VERSION_13.ordinal
 tasks {
     withType<KotlinCompile> {
         kotlinOptions {
@@ -88,15 +59,3 @@ artifacts {
     add("archives", tasks["jar"])
     add("archives", tasks["sourceJar"])
 }
-
-val testMain = tasks.register("testMain", JavaExec::class) {
-    main = "io.the.orm.test.AllTestsKt"
-    classpath = sourceSets["test"].runtimeClasspath
-    if (needsRedefinition)
-        jvmArgs = mutableListOf("-XX:+AllowRedefinitionToAddDeleteMethods")
-}
-/* testutil is tested by itest
-tasks.check {
-    dependsOn(testMain)
-}
-*/
