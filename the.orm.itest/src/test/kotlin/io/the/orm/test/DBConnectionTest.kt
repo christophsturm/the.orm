9338cfb775d281525a31a4ba78c1cb49c744aad2
Christoph Sturm <me@christophsturm.com>
2022-01-17 13:25:56 +0100

    add test for batch insert with no null values

--- the.orm.itest/src/test/kotlin/io/the/orm/test/DBConnectionTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/test/DBConnectionTest.kt
@@ -35,6 +35,18 @@ object DBConnectionTest {
                     connection.createInsertStatement("insert into users(name, email) values ($1, $2)")
                         .executeBatch(
                             listOf(String::class.java, String::class.java),
+                            sequenceOf(sequenceOf("belle", "belle@bs.com"), sequenceOf("sebastian", "seb@bs.com"))
+                        )
+                        .map { it.getId() }.toList()
+                }
+            expectThat(result).isEqualTo(listOf(1, 2))
+        }
+        it("can insert multiple rows with one command even null") {
+            val result =
+                createConnectionProvider().withConnection { connection ->
+                    connection.createInsertStatement("insert into users(name, email) values ($1, $2)")
+                        .executeBatch(
+                            listOf(String::class.java, String::class.java),
                             sequenceOf(sequenceOf("belle", null), sequenceOf("sebastian", null))
                         )
                         .map { it.getId() }.toList()
