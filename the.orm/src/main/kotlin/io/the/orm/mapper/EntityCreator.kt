3f1e226b0439d0f2ee3ba59a0075bb488eaed942
Christoph Sturm <me@christophsturm.com>
2022-10-02 11:18:13 +0200

    extract friendlyString function

--- the.orm/src/main/kotlin/io/the/orm/mapper/EntityCreator.kt
+++ the.orm/src/main/kotlin/io/the/orm/mapper/EntityCreator.kt
@@ -4,6 +4,7 @@ import io.the.orm.RepositoryException
 import io.the.orm.internal.classinfo.ClassInfo
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlin.reflect.KParameter
 
 internal class EntityCreator<Entity : Any>(private val classInfo: ClassInfo<Entity>) {
     fun toEntities(parameters: Flow<List<ResultPair>>): Flow<Entity> {
@@ -17,9 +18,12 @@ internal class EntityCreator<Entity : Any>(private val classInfo: ClassInfo<Enti
                 classInfo.constructor.callBy(it)
             } catch (e: Exception) {
                 throw RepositoryException(
-                    "error invoking constructor for ${classInfo.name}.\n parameters:${it.entries.joinToString { it.key.name+"=>"+it.value }}", e
+                    "error invoking constructor for ${classInfo.name}.\n parameters:${it.friendlyString()}", e
                 )
             }
         }
     }
+
+    private fun Map<KParameter, Any?>.friendlyString() =
+        entries.joinToString { it.key.name + "=>" + it.value }
 }
