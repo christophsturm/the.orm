7ce9d0ba928a1e211bfeddd4ec36521ffe7d588c
Christoph Sturm <me@christophsturm.com>
2022-10-03 12:46:26 +0200

    mock out the HasMany API

--- the.orm/src/main/kotlin/io/the/orm/exp/relations/HasMany.kt
+++ the.orm/src/main/kotlin/io/the/orm/exp/relations/HasMany.kt
@@ -2,8 +2,58 @@
 
 package io.the.orm.exp.relations
 
+import io.the.orm.dbio.ConnectionProvider
+
 /*
 Relations support, not yet finished
  */
 
-class HasMany<Entity : Any>
+interface HasMany<Entity : Any> : Set<Entity> {
+    // the mutability functions all need a connection provider because they write to the db
+    fun add(connectionProvider: ConnectionProvider, entity: Entity)
+    fun remove(connectionProvider: ConnectionProvider, entity: Entity)
+    fun replace(connectionProvider: ConnectionProvider, newSet: Set<Entity>)
+    companion object {
+        operator fun <Entity : Any> invoke(): HasMany<Entity> {
+            return Impl()
+        }
+
+        class Impl<Entity : Any> : HasMany<Entity> {
+            override fun add(connectionProvider: ConnectionProvider, entity: Entity) {
+                TODO("Not yet implemented")
+            }
+
+            override fun remove(connectionProvider: ConnectionProvider, entity: Entity) {
+                TODO("Not yet implemented")
+            }
+
+            override fun replace(connectionProvider: ConnectionProvider, newSet: Set<Entity>) {
+                TODO("Not yet implemented")
+            }
+
+            override val size: Int
+                get() = TODO("Not yet implemented")
+
+            override fun isEmpty(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun iterator(): Iterator<Entity> {
+                TODO("Not yet implemented")
+            }
+
+            override fun containsAll(elements: Collection<Entity>): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun contains(element: Entity): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+}
+
+/*
+ we will also need an ordered HasMany that implements a list.
+ */
+interface HasManyList<Entity : Any> : List<Entity>
