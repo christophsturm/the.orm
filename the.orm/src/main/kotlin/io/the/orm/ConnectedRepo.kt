1f04740dee766ccca7ce196ba0945564fd765dbd
Christoph Sturm <me@christophsturm.com>
2022-11-10 17:51:35 +0100

    use delegate to remove duplication

--- the.orm/src/main/kotlin/io/the/orm/ConnectedRepo.kt
+++ the.orm/src/main/kotlin/io/the/orm/ConnectedRepo.kt
@@ -9,9 +9,9 @@ interface ConnectedRepo<T : Any> {
 
     companion object {
         inline fun <reified T : Any> create(connection: ConnectionProvider): ConnectedRepo<T> =
-            ConnectedRepoImpl(RepoImpl(T::class), connection)
+            Impl(RepoImpl(T::class), connection)
         operator fun <T : Any> invoke(repo: Repo<T>, connectionProvider: ConnectionProvider) =
-            ConnectedRepoImpl(repo, connectionProvider)
+            Impl(repo, connectionProvider)
     }
 
     suspend fun create(entity: T): T
@@ -19,14 +19,13 @@ interface ConnectedRepo<T : Any> {
     suspend fun update(entity: T)
 
     suspend fun findById(pk: PK): T
-}
+    class Impl<T : Any>(override val repo: Repo<T>, override val connectionProvider: ConnectionProvider) :
+        ConnectedRepo<T> {
 
-class ConnectedRepoImpl<T : Any>(override val repo: Repo<T>, override val connectionProvider: ConnectionProvider) :
-    ConnectedRepo<T> {
-
-    override suspend fun create(entity: T): T = repo.create(connectionProvider, entity)
-    override suspend fun update(entity: T): Unit = repo.update(connectionProvider, entity)
-    override suspend fun findById(pk: PK): T = repo.findById(connectionProvider, pk)
+        override suspend fun create(entity: T): T = repo.create(connectionProvider, entity)
+        override suspend fun update(entity: T): Unit = repo.update(connectionProvider, entity)
+        override suspend fun findById(pk: PK): T = repo.findById(connectionProvider, pk)
+    }
 }
 
 interface TransactionalRepo<T : Any> : ConnectedRepo<T> {
@@ -41,16 +40,16 @@ interface TransactionalRepo<T : Any> : ConnectedRepo<T> {
     suspend fun <R> transaction(function: suspend (ConnectedRepo<T>) -> R): R
 }
 
-class TransactionalRepoImpl<T : Any>(
+class TransactionalRepoImpl<T : Any>private constructor(
     override val repo: Repo<T>,
-    override val connectionProvider: TransactionProvider
-) : TransactionalRepo<T> {
-    override suspend fun create(entity: T): T = repo.create(connectionProvider, entity)
-    override suspend fun update(entity: T): Unit = repo.update(connectionProvider, entity)
-    override suspend fun findById(pk: PK): T = repo.findById(connectionProvider, pk)
+    override val connectionProvider: TransactionProvider,
+    private val connectedRepo: ConnectedRepo<T>
+) : TransactionalRepo<T>, ConnectedRepo<T> by connectedRepo {
+    constructor(repo: Repo<T>, connectionProvider: TransactionProvider) :
+        this(repo, connectionProvider, ConnectedRepo(repo, connectionProvider))
 
     override suspend fun <R> transaction(function: suspend (ConnectedRepo<T>) -> R): R =
         connectionProvider.transaction { transactionConnectionProvider ->
-            function(ConnectedRepoImpl(repo, transactionConnectionProvider))
+            function(ConnectedRepo.Impl(repo, transactionConnectionProvider))
         }
 }
