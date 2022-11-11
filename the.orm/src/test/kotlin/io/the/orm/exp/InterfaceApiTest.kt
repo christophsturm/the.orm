8fc657f8e258dd91bb97ffaa14e16e5bef7138e4
Christoph Sturm <me@christophsturm.com>
2022-11-11 10:40:36 +0100

    just thinking out loud about an interface based api

---
+++ the.orm/src/test/kotlin/io/the/orm/exp/InterfaceApiTest.kt
@@ -0,0 +1,22 @@
+package io.the.orm.exp
+
+/* interface based api, to support inheritance.
+*
+* currently unsure how to best create entities with that api, we will need some kind of impl or factory method.
+*/
+
+object Entities {
+    interface Loginable {
+        val userName: String
+        val hashedPassword: String
+    }
+    interface Likable {
+        val likedBy: List<Liker>
+    }
+    interface Liker
+
+    @Entity
+    interface User : Loginable, Likable, Liker
+}
+
+annotation class Entity
