3fe664f037bdda939ce35e646d3f716239c06fcf
Christoph Sturm <me@christophsturm.com>
2022-12-04 00:10:09 +0100

    support 3 entities

--- the.orm/src/main/kotlin/io/the/orm/transaction/RepoTransactionProvider.kt
+++ the.orm/src/main/kotlin/io/the/orm/transaction/RepoTransactionProvider.kt
@@ -34,4 +34,20 @@ class RepoTransactionProvider(private val repos: MultiRepo, private val transact
             t(connectedRepo1, connectedRepo2)
         }
     }
+    suspend fun <Entity1 : Any, Entity2 : Any, Entity3 : Any, Result> transaction(
+        clazz1: KClass<Entity1>,
+        clazz2: KClass<Entity2>,
+        clazz3: KClass<Entity3>,
+        t: suspend (ConnectedRepo<Entity1>, ConnectedRepo<Entity2>, ConnectedRepo<Entity3>) -> Result
+    ): Result {
+        val repo = repos.getRepo(clazz1)
+        val repo2 = repos.getRepo(clazz2)
+        val repo3 = repos.getRepo(clazz3)
+        return transactionProvider.transaction {
+            val connectedRepo1 = ConnectedRepo(repo, it)
+            val connectedRepo2 = ConnectedRepo(repo2, it)
+            val connectedRepo3 = ConnectedRepo(repo3, it)
+            t(connectedRepo1, connectedRepo2, connectedRepo3)
+        }
+    }
 }
--- the.orm/src/test/kotlin/io/the/orm/transaction/RepoTransactionProviderTest.kt
+++ the.orm/src/test/kotlin/io/the/orm/transaction/RepoTransactionProviderTest.kt
@@ -2,11 +2,9 @@ package io.the.orm.transaction
 
 import failgood.Test
 import failgood.describe
-import failgood.mock.mock
 import io.the.orm.ConnectedRepo
 import io.the.orm.MultiRepo
 import io.the.orm.PK
-import io.the.orm.Repo
 import io.the.orm.exp.testing.MockConnectionProvider
 import io.the.orm.exp.testing.MockTransactionProvider
 import kotlinx.coroutines.delay
@@ -15,47 +13,57 @@ import kotlin.test.assertEquals
 @Test
 object RepoTransactionProviderTest {
     val context = describe<RepoTransactionProvider> {
-        data class Entity(val name: String, val id: PK?)
         data class Entity1(val name: String, val id: PK?)
+        data class Entity2(val name: String, val id: PK?)
+        data class Entity3(val name: String, val id: PK?)
+
+        var passedRepo1: ConnectedRepo<Entity1>? = null
+        var passedRepo2: ConnectedRepo<Entity2>? = null
+        var passedRepo3: ConnectedRepo<Entity3>? = null
+        val repos = MultiRepo(listOf(Entity1::class, Entity2::class, Entity3::class))
+        val connectionProvider = MockConnectionProvider()
+        val r = RepoTransactionProvider(repos, MockTransactionProvider(connectionProvider))
         it("can start a transaction with one repo") {
-
-            val repo = mock<Repo<Entity>>()
-            var passedRepo: ConnectedRepo<Entity>? = null
-            val repos = mock<MultiRepo> {
-                method { getRepo(Entity::class) }.returns(repo)
-            }
-
-            val connectionProvider = MockConnectionProvider()
-            val r = RepoTransactionProvider(repos, MockTransactionProvider(connectionProvider))
-            val result = r.transaction { entityRepo: ConnectedRepo<Entity> ->
+            val result = r.transaction { entity1Repo: ConnectedRepo<Entity1> ->
                 delay(0)
-                passedRepo = entityRepo
-                "result"
-            }
-            assertEquals("result", result)
-            assert(repo == passedRepo?.repo)
-            assert(connectionProvider == passedRepo?.connectionProvider)
-        }
-        it("can start a transaction with two repo") {
-            var passedRepo: ConnectedRepo<Entity>? = null
-            var passedRepo1: ConnectedRepo<Entity1>? = null
-            val repos = MultiRepo(listOf(Entity::class, Entity1::class))
-
-            val connectionProvider = MockConnectionProvider()
-            val r = RepoTransactionProvider(repos, MockTransactionProvider(connectionProvider))
-            val result = r.transaction(
-                Entity::class,
-                Entity1::class
-            ) { entityRepo: ConnectedRepo<Entity>, entity1Repo: ConnectedRepo<Entity1> ->
-                delay(0)
-                passedRepo = entityRepo
                 passedRepo1 = entity1Repo
                 "result"
             }
             assertEquals("result", result)
-            assert(repos.getRepo(Entity::class) == passedRepo?.repo)
             assert(repos.getRepo(Entity1::class) == passedRepo1?.repo)
-            assert(connectionProvider == passedRepo?.connectionProvider)
+            assert(connectionProvider == passedRepo1?.connectionProvider)
+        }
+        it("can start a transaction with two repo") {
+            val result = r.transaction(
+                Entity1::class, Entity2::class
+            ) { entity1Repo: ConnectedRepo<Entity1>, entity2Repo: ConnectedRepo<Entity2> ->
+                delay(0)
+                passedRepo1 = entity1Repo
+                passedRepo2 = entity2Repo
+                "result"
+            }
+            assertEquals("result", result)
+            assert(repos.getRepo(Entity1::class) == passedRepo1?.repo)
+            assert(repos.getRepo(Entity2::class) == passedRepo2?.repo)
+            assert(connectionProvider == passedRepo1?.connectionProvider)
+        }
+        it("can start a transaction with three repo") {
+            val result = r.transaction(
+                Entity1::class, Entity2::class, Entity3::class
+            ) { entity1Repo: ConnectedRepo<Entity1>,
+                entity2Repo: ConnectedRepo<Entity2>,
+                entity3Repo: ConnectedRepo<Entity3> ->
+                delay(0)
+                passedRepo1 = entity1Repo
+                passedRepo2 = entity2Repo
+                passedRepo3 = entity3Repo
+                "result"
+            }
+            assertEquals("result", result)
+            assert(repos.getRepo(Entity1::class) == passedRepo1?.repo)
+            assert(repos.getRepo(Entity2::class) == passedRepo2?.repo)
+            assert(repos.getRepo(Entity3::class) == passedRepo3?.repo)
+            assert(connectionProvider == passedRepo1?.connectionProvider)
         }
     }
 }
