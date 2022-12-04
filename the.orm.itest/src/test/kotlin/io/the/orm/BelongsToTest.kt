73fcaebe69e6097c1c847dbbd517d5bce7cbb62b
Christoph Sturm <me@christophsturm.com>
2022-12-04 23:31:10 +0100

    reassure that loading belongs to relation works.

--- the.orm.itest/src/test/kotlin/io/the/orm/BelongsToTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/BelongsToTest.kt
@@ -1,24 +1,41 @@
 package io.the.orm
 
-import failgood.describe
+import failgood.Test
 import io.the.orm.exp.relations.BelongsTo
 import io.the.orm.test.describeOnAllDbs
+import io.the.orm.transaction.RepoTransactionProvider
 
+@Test
 object BelongsToTest {
-    data class Page(
-        val id: Long?,
-        val url: String,
-        val title: String?,
-        val description: String?,
-        val ldJson: String?,
-        val author: String?
-    )
+    // this test uses the same schema as the HasManyTest but declared as BelongsTo instead of hasMany
 
-    data class Recipe(val id: Long?, val name: String, val description: String?, val page: Page)
+    val context = describeOnAllDbs<BelongsTo<*>>(schema = HasManyTest.SCHEMA) {
+        val connection = it()
+        describe("with always eager loading (declared as the Entity instead of BelongsTo<Entity>)") {
+            data class Book(val name: String, val id: PK? = null)
+            data class Chapter(val name: String, val book: Book, val id: PK? = null)
+            data class Sentence(val content: String, val chapter: Chapter, val id: PK? = null)
 
-    val repo = RepoRegistry(listOf(HasManyTest.Page::class, HasManyTest.Book::class))
-
-    val tests = describeOnAllDbs<BelongsTo<*>>(schema = HasManyTest.SCHEMA) {
-
+            val repo = RepoRegistry(listOf(Chapter::class, Book::class, Sentence::class))
+            val repoProvider = RepoTransactionProvider(repo, connection)
+            it("can load recursive belongs to relations") {
+                repoProvider.transaction(
+                    Book::class,
+                    Chapter::class,
+                    Sentence::class
+                ) { bookRepo, chapterRepo, sentenceRepo ->
+                    val book = bookRepo.create(Book("TDD is ok"))
+                    val chapter = chapterRepo.create(Chapter("Waterfalls are awful", book))
+                    val sentence =
+                        sentenceRepo.create(Sentence("Except the Niagara falls, everbody loves those", chapter))
+                    val loadedSentence = sentenceRepo.findById(sentence.id!!)
+                    assert(sentence !== loadedSentence) // should be a new instance.
+                    with(sentence.chapter) {
+                        assert(name == "Waterfalls are awful")
+                        assert(sentence.content == "Except the Niagara falls, everbody loves those")
+                    }
+                }
+            }
+        }
     }
 }
--- the.orm.itest/src/test/kotlin/io/the/orm/HasManyTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/HasManyTest.kt
@@ -11,8 +11,8 @@ import io.the.orm.transaction.RepoTransactionProvider
 @Test
 object HasManyTest {
     data class Sentence(val content: String, val id: PK? = null)
-    data class Page(val name: String, val sentences: HasMany<Sentence>, val id: PK? = null)
-    data class Book(val name: String, val nestedEntities: HasMany<Page>, val id: PK? = null)
+    data class Chapter(val name: String, val sentences: HasMany<Sentence>, val id: PK? = null)
+    data class Book(val name: String, val chapters: HasMany<Chapter>, val id: PK? = null)
 
     const val SCHEMA = """
     create sequence book_seq no maxvalue;
@@ -22,51 +22,59 @@ create table books
     name           varchar(100) not null
 );
 
-    create sequence page_seq no maxvalue;
-create table pages
+    create sequence chapter_seq no maxvalue;
+create table chapters
 (
-    id             bigint       not null default nextval('page_seq') primary key,
+    id             bigint       not null default nextval('chapter_seq') primary key,
     book_id      bigint       not null,
     foreign key (book_id) references books (id),
     name           varchar(100) not null
 );
     create sequence sentence_seq no maxvalue;
-create table sentence
+create table sentences
 (
     id             bigint       not null default nextval('sentence_seq') primary key,
-    page_id      bigint       not null,
-    foreign key (page_id) references pages (id),
+    chapter_id      bigint       not null,
+    foreign key (chapter_id) references chapters (id),
     content           text not null
 );
 
 """
 
-    val repo = RepoRegistry(listOf(Sentence::class, Page::class, Book::class))
+    val repo = RepoRegistry(listOf(Sentence::class, Chapter::class, Book::class))
     val context = describeOnAllDbs<HasMany<*>>(schema = SCHEMA) {
         it("can create an page with nested entities") {
-            val pages: Set<Page> = setOf(
-                Page(
+            val chapters: Set<Chapter> = setOf(
+                Chapter(
                     "page 1", hasMany(
                         setOf(
                             Sentence("god is dead"),
-                            Sentence("No small art is it to sleep: it is necessary for that purpose to keep awake all day.")
+                            Sentence(
+                                "No small art is it to sleep:" +
+                                    " it is necessary for that purpose to keep awake all day."
+                            )
                         )
                     )
                 ),
-                Page(
+                Chapter(
                     "page 2",
-                    hasMany(setOf(Sentence("god is still doing pretty badly"), Sentence("sleeping is still not easy")))
+                    hasMany(
+                        setOf(
+                            Sentence("god is still doing pretty badly"),
+                            Sentence("sleeping is still not easy")
+                        )
+                    )
                 )
             )
             val holder = Book(
                 "Also Sprach Zarathustra",
-                hasMany(pages)
+                hasMany(chapters)
             )
-            RepoTransactionProvider(repo, it()).transaction(Book::class, Page::class) { bookRepo, pageRepo ->
+            RepoTransactionProvider(repo, it()).transaction(Book::class, Chapter::class) { bookRepo, pageRepo ->
                 bookRepo.create(holder)
                 // this is a hack to load all entities. query api really needs to be rethought
                 val entities =
-                    pageRepo.queryFactory.createQuery(Page::name.isNotNull()).with(pageRepo.connectionProvider, Unit)
+                    pageRepo.queryFactory.createQuery(Chapter::name.isNotNull()).with(pageRepo.connectionProvider, Unit)
                         .find()
                 if (System.getenv("NEXT") != null)
                     assert(entities.map { it.name }.containsExactlyInAnyOrder("page 1", "page 2"))
