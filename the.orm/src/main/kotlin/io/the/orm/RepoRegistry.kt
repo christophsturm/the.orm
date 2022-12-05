2d2c2366baa2c1a468f77a86129942249d167c18
Christoph Sturm <me@christophsturm.com>
2022-12-05 17:42:28 +0100

    WIP

--- the.orm/src/main/kotlin/io/the/orm/RepoRegistry.kt
+++ the.orm/src/main/kotlin/io/the/orm/RepoRegistry.kt
@@ -1,10 +1,12 @@
 package io.the.orm
 
+import io.the.orm.internal.classinfo.ClassInfo
 import kotlin.reflect.KClass
 
 data class RepoRegistry(val entityRepos: Map<KClass<out Any>, Repo<out Any>>) {
     companion object {
         operator fun invoke(classes: List<KClass<out Any>>): RepoRegistry {
+            val classInfo = classes.associateBy({it}) {ClassInfo(it)}
             val entityRepos: Map<KClass<out Any>, Repo<out Any>> =
                 classes.associateBy({ it }, { RepoImpl(it, classes.toSet()) })
             return RepoRegistry(entityRepos)
