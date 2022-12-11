5777bb71f7d7f5d91ca356fcd0f695fb01a12ffd
Christoph Sturm <me@christophsturm.com>
2022-12-11 01:04:40 +0100

    implement most of belongsTo lazy loading

--- the.orm.itest/src/test/kotlin/io/the/orm/BelongsToTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/BelongsToTest.kt
@@ -2,8 +2,10 @@ package io.the.orm
 
 import failgood.Test
 import io.the.orm.exp.relations.BelongsTo
+import io.the.orm.exp.relations.belongsTo
 import io.the.orm.test.describeOnAllDbs
 import io.the.orm.transaction.RepoTransactionProvider
+import kotlin.test.assertNotNull
 
 @Test
 object BelongsToTest {
@@ -30,8 +32,52 @@ object BelongsToTest {
                         sentenceRepo.create(Sentence("Except the Niagara falls, everbody loves those", chapter))
                     val loadedSentence = sentenceRepo.findById(sentence.id!!)
                     assert(sentence !== loadedSentence) // should be a new instance.
-                    with(sentence.chapter) {
+                    with(loadedSentence.chapter) {
                         assert(name == "Waterfalls are awful")
+                        assert(book.name == "TDD is ok")
+                    }
+                    assert(loadedSentence.content == "Except the Niagara falls, everbody loves those")
+                }
+            }
+        }
+        describe("when lazy loading is supported (BelongsTo<Entity>)") {
+            data class Book(val name: String, val id: PK? = null)
+            data class Chapter(val name: String, val book: BelongsTo<Book>, val id: PK? = null)
+            data class Sentence(val content: String, val chapter: BelongsTo<Chapter>, val id: PK? = null)
+
+            val repo = RepoRegistry(setOf(Chapter::class, Book::class, Sentence::class))
+            val repoProvider = RepoTransactionProvider(repo, connection)
+            val sentence = repoProvider.transaction(
+                Book::class,
+                Chapter::class,
+                Sentence::class
+            ) { bookRepo, chapterRepo, sentenceRepo ->
+                val book = bookRepo.create(Book("TDD is ok"))
+                val chapter = chapterRepo.create(Chapter("Waterfalls are awful", belongsTo(book)))
+                sentenceRepo.create(Sentence("Except the Niagara falls, everbody loves those", belongsTo(chapter)))
+            }
+
+            it("does not load belongs to relations by default") {
+                repoProvider.transaction(
+                    Sentence::class
+                ) { sentenceRepo ->
+                    val loadedSentence =
+                        sentenceRepo.findById(sentence.id!!)
+                    assert(sentence !== loadedSentence) // should be a new instance.
+                    assert(loadedSentence.content == "Except the Niagara falls, everbody loves those")
+                    val result = kotlin.runCatching { loadedSentence.chapter.get() }
+                    assertNotNull(result.exceptionOrNull())
+                }
+            }
+            it("can load recursive belongs to relations when specified in fetchRelations") {
+                repoProvider.transaction(
+                    Sentence::class,
+                ) { sentenceRepo ->
+                    val loadedSentence =
+                        sentenceRepo.findById(sentence.id!!, fetchRelations = setOf(Sentence::chapter, Chapter::book))
+                    assert(sentence !== loadedSentence) // should be a new instance.
+                    with(loadedSentence.chapter) {
+                        assert(get().name == "Waterfalls are awful")
                         assert(sentence.content == "Except the Niagara falls, everbody loves those")
                     }
                 }
--- the.orm/src/main/kotlin/io/the/orm/exp/relations/BelongsTo.kt
+++ the.orm/src/main/kotlin/io/the/orm/exp/relations/BelongsTo.kt
@@ -7,22 +7,36 @@ interface Relation
 
 sealed interface BelongsTo<Entity : Any> : Relation {
     fun id(): PK
+    fun get(): Entity
     class BelongsToImpl<Entity : Any>(val entity: Entity) : BelongsTo<Entity> {
-        operator fun invoke(): Entity = entity
         override fun id(): PK {
             return 0
+        }
+
+        override fun get(): Entity {
+            return entity
         }
     }
     class BelongsToNotLoaded<Entity : Any>(c: KClass<Entity>, val pk: PK) : BelongsTo<Entity> {
         override fun id(): PK {
             return pk
         }
+
+        override fun get(): Entity {
+            throw RelationNotLoadedException()
+        }
     }
     data class Auto<Entity : Any>(var id: PK? = null) : BelongsTo<Entity> {
         override fun id(): PK {
             return id ?: throw RepositoryException("id missing")
         }
+        override fun get(): Entity {
+            throw RelationNotLoadedException()
+        }
     }
 }
+
+class RelationNotLoadedException : RepositoryException("not loaded")
+
 fun <Entity : Any> belongsTo() = BelongsTo.Auto<Entity>()
 fun <Entity : Any> belongsTo(entity: Entity) = BelongsTo.BelongsToImpl(entity)
