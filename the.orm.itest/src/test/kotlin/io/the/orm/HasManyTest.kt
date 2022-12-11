4254045faaa3ca58b8c40a9a70eea3ed9dfd3b2f
Christoph Sturm <me@christophsturm.com>
2022-12-11 20:18:04 +0100

    move schema to top

--- the.orm.itest/src/test/kotlin/io/the/orm/HasManyTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/HasManyTest.kt
@@ -13,16 +13,6 @@ import kotlin.test.assertEquals
 
 @Test
 object HasManyTest {
-    data class Sentence(val content: String, val chapter: BelongsTo<Chapter> = belongsTo(), val id: PK? = null)
-    data class Chapter(
-        val name: String,
-        val sentences: HasMany<Sentence>,
-        val book: BelongsTo<Book> = belongsTo(),
-        val id: PK? = null
-    )
-
-    data class Book(val name: String, val chapters: HasMany<Chapter>, val id: PK? = null)
-
     const val SCHEMA = """
     create sequence book_seq no maxvalue;
 create table books
@@ -48,10 +38,21 @@ create table sentences
     content           text not null
 );
 
-"""/*
+"""
+    data class Sentence(val content: String, val chapter: BelongsTo<Chapter> = belongsTo(), val id: PK? = null)
+    data class Chapter(
+        val name: String,
+        val sentences: HasMany<Sentence>,
+        val book: BelongsTo<Book> = belongsTo(),
+        val id: PK? = null
+    )
+
+    data class Book(val name: String, val chapters: HasMany<Chapter>, val id: PK? = null)
+
+
+    /*
     for has many we really need recursive saving because we don't know the id when we create the objects
      */
-
     val context = describeOnAllDbs<HasMany<*>>(schema = SCHEMA) {
         val repo = RepoRegistry(setOf(Sentence::class, Chapter::class, Book::class))
         it("can create an page with nested entities") {
