298ba941f3144d66194b4762dafdeef1a9220434
Christoph Sturm <me@christophsturm.com>
2023-05-06 23:34:14 +0200

    dep bump

--- buildSrc/src/main/kotlin/io/the/orm/versions/versions.kt
+++ buildSrc/src/main/kotlin/io/the/orm/versions/versions.kt
@@ -1,12 +1,12 @@
 package io.the.orm.versions
 
-const val kotlinVersion = "1.8.10"
+const val kotlinVersion = "1.8.21"
 const val failgoodVersion = "0.8.3"
 const val striktVersion = "0.34.1"
-const val testContainersVersion = "1.17.6"
-const val blockHoundVersion = "1.0.7.RELEASE"
-const val coroutinesVersion = "1.6.4"
-const val nettyVersion = "4.1.91.Final"
+const val testContainersVersion = "1.18.0"
+const val blockHoundVersion = "1.0.8.RELEASE"
+const val coroutinesVersion = "1.7.0"
+const val nettyVersion = "4.1.92.Final"
 const val serializationVersion = "1.5.0"
 const val vertxVersion = "4.4.1"
 const val log4j2Version = "2.20.0"
