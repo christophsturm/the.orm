490f6e288e58966f1a55d7fd437981dde8dfc719
Christoph Sturm <me@christophsturm.com>
2023-12-17 23:39:00 +0100

    bump max_connections

--- the.orm.testutil/src/main/kotlin/io/the/orm/test/PSQLContainer.kt
+++ the.orm.testutil/src/main/kotlin/io/the/orm/test/PSQLContainer.kt
@@ -19,11 +19,14 @@ class LazyPSQLContainer(
     fun prepare() {
         dockerContainer
     }
+
     val dockerContainer: PostgresqlContainer by lazy {
         PostgresqlContainer(dockerImage, databasePrefix, reuse)
     }
+
     suspend fun preparePostgresDB(): PostgresDb = dockerContainer.preparePostgresDB()
 }
+
 class PostgresqlContainer(
     dockerImage: String,
     private val databasePrefix: String,
@@ -32,7 +35,7 @@ class PostgresqlContainer(
     private val dockerContainer: PostgreSQLContainer<Nothing> =
         measureTimedValue {
             PostgreSQLContainer<Nothing>(dockerImage).apply {
-// WIP           setCommand("postgres", "-c", "fsync=off", "-c", "max_connections=200")
+                setCommand("postgres", "-c", "fsync=off", "-c", "max_connections=20000")
                 withReuse(reuse)
                 start()
             }
@@ -46,9 +49,12 @@ class PostgresqlContainer(
         .setDatabase("postgres")
         .setUser("test")
         .setPassword("test")
-    private val pool = PgBuilder.pool().with(PoolOptions().setMaxSize(5)).connectingTo(connectOptions).build()!!
+    private val pool =
+        PgBuilder.pool().with(PoolOptions().setMaxSize(5)).connectingTo(connectOptions).using(vertx).build()!!
+
     suspend fun preparePostgresDB(): PostgresDb = postgresDb(databasePrefix, port, host, pool)
 }
+
 internal suspend fun postgresDb(prefix: String, port: Int, host: String, pool: Pool): PostgresDb {
     val uuid = UUID.randomUUID().toString().take(5)
     val databaseName = "$prefix$uuid".replace("-", "_")
