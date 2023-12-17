3c7a3f57e623e155d2c9ecf1a07e3fb0e9dc6079
Christoph Sturm <me@christophsturm.com>
2023-12-17 23:39:36 +0100

    introduce a fixture in the VertxTest

--- the.orm.itest/src/test/kotlin/io/the/orm/vertx/VertxTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/vertx/VertxTest.kt
@@ -7,12 +7,14 @@ import io.the.orm.test.DBS
 import io.the.orm.test.TestUtilConfig
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.pgclient.PgBuilder
+import io.vertx.sqlclient.Pool
 import io.vertx.sqlclient.PoolOptions
+import io.vertx.sqlclient.PreparedQuery
 import io.vertx.sqlclient.Row
 import io.vertx.sqlclient.RowSet
-import io.vertx.sqlclient.SqlClient
 import io.vertx.sqlclient.SqlConnectOptions
 import io.vertx.sqlclient.Tuple
+import kotlinx.coroutines.runBlocking
 import strikt.api.expectThat
 import strikt.assertions.containsExactly
 import strikt.assertions.isEqualTo
@@ -21,77 +23,99 @@ import strikt.assertions.isEqualTo
 this test is for experimenting with the vertx psql client. it uses the vertx api directly to try out and show
 how things work.
  */
-@Suppress("SqlNoDataSourceInspection", "SqlResolve")
+@Suppress("SqlNoDataSourceInspection")
+class VertxClientFixture : AutoCloseable {
+    val db = runBlocking { DBS.psql16.preparePostgresDB() }
+    val client = runBlocking { createClient() }
+
+    private suspend fun createClient(): Pool {
+        val connectOptions = SqlConnectOptions()
+            .setPort(db.port)
+            .setHost(db.host)
+            .setDatabase(db.databaseName)
+            .setUser("test")
+            .setPassword("test")
+        return PgBuilder.pool().with(PoolOptions().setMaxSize(5)).connectingTo(connectOptions).build()!!.also {
+            it.query(
+                """create sequence users_id_seq no maxvalue;
+                                        create table users
+                        (
+                            id             bigint       not null default nextval('users_id_seq') primary key,
+                            name           varchar(100) not null,
+                            email          varchar(100) unique,
+                            is_cool        boolean,
+                            bio            text,
+                            favorite_color varchar(10),
+                            birthday       date,
+                            weight         decimal(5, 2),
+                            balance        decimal(5, 2)
+                        );
+                        """
+            ).execute().coAwait()
+        }
+    }
+
+    override fun close() {
+        db.close()
+        client.close()
+    }
+
+    suspend fun query(sql: String) = client.query(sql).execute().coAwait()
+    suspend fun preparedQuery(query: String, tuple: Tuple) = preparedQuery(query).execute(tuple).coAwait()
+
+    fun preparedQuery(query: String): PreparedQuery<RowSet<Row>> = client.preparedQuery(query)
+}
+
 @Test
 class VertxTest {
-    @Suppress("NAME_SHADOWING")
     val context = describe(
         "vertx sql client api",
-        ignored = if (TestUtilConfig.H2_ONLY) Ignored.Because("Running in h2 only mode") else null
+        ignored = if (TestUtilConfig.H2_ONLY) Ignored.Because("Running in h2 only mode") else null,
+        given = { VertxClientFixture() }
     ) {
-        val db by dependency({ DBS.psql16.preparePostgresDB() }) { it.close() }
-
-        val client: SqlClient by dependency({
-            val connectOptions = SqlConnectOptions()
-                .setPort(db.port)
-                .setHost(db.host)
-                .setDatabase(db.databaseName)
-                .setUser("test")
-                .setPassword("test")
-            PgBuilder.pool().with(PoolOptions().setMaxSize(5)).connectingTo(connectOptions).build()!!.also {
-                it.query(
-                    """create sequence users_id_seq no maxvalue;
-                            create table users
-            (
-                id             bigint       not null default nextval('users_id_seq') primary key,
-                name           varchar(100) not null,
-                email          varchar(100) unique,
-                is_cool        boolean,
-                bio            text,
-                favorite_color varchar(10),
-                birthday       date,
-                weight         decimal(5, 2),
-                balance        decimal(5, 2)
-            );
-            """
-                ).execute().coAwait()
-            }
-        }) { it.close() }
         it("can run sql queries") {
-            val result: RowSet<Row> = client.query("SELECT * FROM users WHERE id=1").execute().coAwait()
+            val result: RowSet<Row> = given.query("SELECT * FROM users WHERE id=1")
             expectThat(result.size()).isEqualTo(0)
         }
         it("can run prepared queries") {
-            val result: RowSet<Row> =
-                client.preparedQuery("SELECT * FROM users WHERE id=$1").execute(Tuple.of(1)).coAwait()
+            val result: RowSet<Row> = given.preparedQuery("SELECT * FROM users WHERE id=$1", Tuple.of(1))
             expectThat(result.size()).isEqualTo(0)
         }
         it("can insert with autoincrement") {
             val result: RowSet<Row> =
-                client.preparedQuery("insert into users(name) values ($1) returning id").execute(Tuple.of("belle"))
-                    .coAwait()
+                given.preparedQuery("insert into users(name) values ($1) returning id", Tuple.of("belle"))
             expectThat(result.size()).isEqualTo(1)
             expectThat(result.columnsNames()).containsExactly("id")
             expectThat(result.single().get(Integer::class.java, "id").toInt()).isEqualTo(1)
         }
-        describe("querying by lists") {
-            // first we insert 3 rows
-            val query = client.preparedQuery("insert into users(name) values ($1) returning id")
+        describe("querying by lists", given = {
+            val fixture = given()
+            // insert 3 rows
+            val query = fixture.preparedQuery("insert into users(name) values ($1) returning id")
             val ids = listOf("ton", "steine", "scherben").map {
                 query.execute(Tuple.of(it)).coAwait().single().get(Integer::class.java, "id").toInt()
             }
             assert(ids == listOf(1, 2, 3))
+            fixture
+        }) {
             it("works with one parameter per item") {
-                val query = client.preparedQuery("SELECT * FROM users WHERE id in ($1, $2)")
-                assert(query.execute(Tuple.from(listOf(1, 2))).coAwait().size() == 2)
+                assert(
+                    given.preparedQuery("SELECT * FROM users WHERE id in ($1, $2)", Tuple.from(listOf(1, 2)))
+                        .size() == 2
+                )
             }
             it("works with ANY") {
-                val query = client.preparedQuery("SELECT * FROM users WHERE id = ANY($1)")
-                assert(query.execute(Tuple.of(arrayOf(1, 2))).coAwait().size() == 2)
+                assert(
+                    given.preparedQuery("SELECT * FROM users WHERE id = ANY($1)", Tuple.of(arrayOf(1, 2))).size() == 2
+                )
             }
             it("works with unnest") {
-                val query = client.preparedQuery("SELECT * FROM users WHERE id in (select unnest(($1)::bigint[]))")
-                assert(query.execute(Tuple.of(arrayOf(1, 2))).coAwait().size() == 2)
+                assert(
+                    given.preparedQuery(
+                        "SELECT * FROM users WHERE id in (select unnest(($1)::bigint[]))",
+                        Tuple.of(arrayOf(1, 2))
+                    ).size() == 2
+                )
             }
         }
     }
