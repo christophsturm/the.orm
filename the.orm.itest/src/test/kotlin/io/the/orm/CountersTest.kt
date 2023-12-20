3430b8874a6a4a8ed6321734a50d97affe752f61
Christoph Sturm <me@christophsturm.com>
2023-12-20 22:33:04 +0100

    disable counters for now

--- the.orm.itest/src/test/kotlin/io/the/orm/CountersTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/CountersTest.kt
@@ -1,10 +1,9 @@
 package io.the.orm
 
-import failgood.Test
 import failgood.describe
 import io.the.orm.test.Counters
 
-@Test
+// @Test no need for the counters right now
 object CountersTest {
     // this test only installs the after suite callback that prints the counters
     val context = describe("counters") { afterSuite { println(Counters) } }
