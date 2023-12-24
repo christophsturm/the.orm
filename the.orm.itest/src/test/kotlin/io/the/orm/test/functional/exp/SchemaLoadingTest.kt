15034053a5617cd43d5506d5ba091ff36710c749
Christoph Sturm <me@christophsturm.com>
2023-12-24 16:38:02 +0100

    add a query that chatgpt told me.

--- the.orm.itest/src/test/kotlin/io/the/orm/test/functional/exp/SchemaLoadingTest.kt
+++ the.orm.itest/src/test/kotlin/io/the/orm/test/functional/exp/SchemaLoadingTest.kt
@@ -1,7 +1,7 @@
 package io.the.orm.test.functional.exp
 
-import failgood.Ignored
 import failgood.Test
+import io.the.orm.dbio.DBConnection
 import io.the.orm.dbio.DBResult
 import io.the.orm.mapper.SimpleResultMapper
 import io.the.orm.test.DBS
@@ -17,23 +17,64 @@ import kotlinx.coroutines.flow.toList
 object SchemaLoadingTest {
     val context =
         DBS.databases
-            .filter { it.dialect != Dialect.H2_R2DBC }
+            .filter { it.dialect == Dialect.POSTGRESQL_VERTX }
             .describeAll("detecting the table structure", given = { it.fixture(USERS_SCHEMA) }) {
-                it("prints result", ignored = Ignored.Because("only for debugging")) {
+                it("prints result") {
                     given.transactionProvider.withConnection { conn ->
+                        /*
                         println(
-                            conn
-                                .createStatement(
-                                    "select column_name, data_type, table_catalog, character_maximum_length," +
-                                        " column_default, is_nullable " +
-                                        "from INFORMATION_SCHEMA.COLUMNS where lower(table_name) = 'users'"
-                                )
-                                .execute()
-                                .asMapFlow()
-                                .toList()
+                            conn.executeToList(
+                                "select column_name, data_type, table_catalog, character_maximum_length, column_default, is_nullable" +
+                                    " from INFORMATION_SCHEMA.COLUMNS where lower(table_name) = 'users'"
+                            )
                         )
+                        println(conn.executeToList("select database()"))*/
                         println(
-                            conn.createStatement("select database()").execute().asMapFlow().toList()
+                            conn.executeToList(
+                                // query straight from <strike>hell</strike>chat-gpt
+                                """SELECT
+    cols.table_name,
+    cols.column_name,
+    cols.data_type,
+    cols.character_maximum_length,
+    cols.is_nullable,
+    cols.column_default,
+    cols.ordinal_position,
+    indexes.index_name,
+    indexes.index_type,
+    indexes.index_columns
+FROM
+    information_schema.columns AS cols
+LEFT JOIN (
+    SELECT
+        idx.indrelid::regclass AS table_name,
+        idx.indexrelid::regclass AS index_name,
+        idx.indisunique,
+        idx.indisprimary,
+        idx.indisclustered,
+        idx.indisvalid,
+        idx.indnkeyatts,
+        idx.indkey,
+        idx.indexrelid,
+        idx.indrelid,
+        string_agg(attname, ', ') AS index_columns,
+        CASE
+            WHEN idx.indisunique THEN 'UNIQUE'
+            WHEN idx.indisprimary THEN 'PRIMARY KEY'
+            WHEN idx.indisclustered THEN 'CLUSTERED'
+            ELSE 'INDEX'
+        END AS index_type
+    FROM
+        pg_index AS idx
+    JOIN
+        pg_attribute AS att ON att.attnum = ANY(idx.indkey) AND att.attrelid = idx.indrelid
+    GROUP BY
+        1, 2, 3, 4, 5, 6, 7, 8, 9
+) AS indexes ON cols.table_name::text = indexes.table_name::text AND cols.column_name = indexes.index_columns
+ORDER BY
+    cols.table_name, cols.ordinal_position;
+"""
+                            )
                         )
                     }
                 }
@@ -79,6 +120,9 @@ object SchemaLoadingTest {
                 }
             }
 }
+
+private suspend fun DBConnection.executeToList(sql: String) =
+    createStatement(sql).execute().asMapFlow().toList()
 
 private suspend inline fun <reified T : Any> DBResult.mapToList(): Flow<T> {
     val mapper = SimpleResultMapper.forClass(T::class)
