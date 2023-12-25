d453363f97f4a28bd31f95bc9d6e213c7c99165e
Christoph Sturm <me@christophsturm.com>
2023-12-25 11:25:01 +0100

    replace deprecated method

--- the.orm/src/test/kotlin/io/the/orm/internal/IDHandlerTest.kt
+++ the.orm/src/test/kotlin/io/the/orm/internal/IDHandlerTest.kt
@@ -2,6 +2,7 @@ package io.the.orm.internal
 
 import failgood.Test
 import failgood.describe
+import failgood.tests
 import io.the.orm.PKType
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -10,20 +11,19 @@ import strikt.assertions.isEqualTo
 class IDHandlerTest {
     data class ClassWithPkId(val id: PKType?, val otherProp: String)
 
-    val context =
-        describe(IDHandler::class) {
-            describe("assigning an id to a newly created instance") {
-                it("assigns a Long id") {
-                    val instance = ClassWithPkId(null, "string")
-                    expectThat(IDHandler(ClassWithPkId::class).copyWithId(instance, 1))
-                        .isEqualTo(instance.copy(id = 1))
-                }
-            }
-            describe("reading the id") {
-                it("reads a Long id") {
-                    val instance = ClassWithPkId(10, "string")
-                    assert(IDHandler(ClassWithPkId::class).readId(instance) == 10L)
-                }
+    val context = tests {
+        describe("assigning an id to a newly created instance") {
+            it("assigns a Long id") {
+                val instance = ClassWithPkId(null, "string")
+                expectThat(IDHandler(ClassWithPkId::class).copyWithId(instance, 1))
+                    .isEqualTo(instance.copy(id = 1))
             }
         }
+        describe("reading the id") {
+            it("reads a Long id") {
+                val instance = ClassWithPkId(10, "string")
+                assert(IDHandler(ClassWithPkId::class).readId(instance) == 10L)
+            }
+        }
+    }
 }
