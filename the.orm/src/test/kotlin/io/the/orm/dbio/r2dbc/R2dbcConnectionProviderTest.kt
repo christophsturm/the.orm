8ec8dc2b71926af7483704a24ce3e39147f5ef2d
Christoph Sturm <me@christophsturm.com>
2023-12-26 19:53:04 +0100

    describe => tests (final round)

--- the.orm/src/test/kotlin/io/the/orm/dbio/r2dbc/R2dbcConnectionProviderTest.kt
+++ the.orm/src/test/kotlin/io/the/orm/dbio/r2dbc/R2dbcConnectionProviderTest.kt
@@ -1,7 +1,7 @@
 package io.the.orm.dbio.r2dbc
 
 import failgood.Test
-import failgood.describe
+import failgood.tests
 import io.r2dbc.pool.ConnectionPool
 import io.r2dbc.pool.ConnectionPoolConfiguration
 import io.r2dbc.spi.ConnectionFactories
@@ -9,19 +9,18 @@ import java.time.Duration
 
 @Test
 class R2dbcConnectionProviderTest {
-    val context =
-        describe(R2DbcDBConnectionFactory::class) {
-            test("creates connections from a connection Pool") {
-                val connectionFactory = ConnectionFactories.get("r2dbc:h2:mem:///any")
-                val pool =
-                    ConnectionPool(
-                        ConnectionPoolConfiguration.builder(connectionFactory)
-                            .maxIdleTime(Duration.ofMillis(1000))
-                            .maxSize(20)
-                            .build()
-                    )
+    val context = tests {
+        test("creates connections from a connection Pool") {
+            val connectionFactory = ConnectionFactories.get("r2dbc:h2:mem:///any")
+            val pool =
+                ConnectionPool(
+                    ConnectionPoolConfiguration.builder(connectionFactory)
+                        .maxIdleTime(Duration.ofMillis(1000))
+                        .maxSize(20)
+                        .build()
+                )
 
-                R2DbcDBConnectionFactory(pool).getConnection()
-            }
+            R2DbcDBConnectionFactory(pool).getConnection()
         }
+    }
 }
--- the.orm/src/test/kotlin/io/the/orm/internal/ExceptionInspectorTest.kt
+++ the.orm/src/test/kotlin/io/the/orm/internal/ExceptionInspectorTest.kt
@@ -1,7 +1,7 @@
 package io.the.orm.internal
 
 import failgood.Test
-import failgood.describe
+import failgood.tests
 import io.r2dbc.spi.R2dbcDataIntegrityViolationException
 import io.the.orm.UniqueConstraintViolatedException
 import io.the.orm.internal.classinfo.Table
@@ -13,25 +13,24 @@ import strikt.assertions.isEqualTo
 class ExceptionInspectorTest {
     data class User(val name: String, val email: String?)
 
-    val context =
-        describe(ExceptionInspector::class) {
-            it("works") {
-                val user = User(name = "chris", email = "email-value")
+    val context = tests {
+        it("works") {
+            val user = User(name = "chris", email = "email-value")
 
-                val message =
-                    """Eindeutiger Index oder Primärschlüssel verletzt: "PUBLIC.CONSTRAINT_INDEX_4 ON PUBLIC.USERS(EMAIL NULLS FIRST) VALUES ( /* 1 */ 'email' )"
+            val message =
+                """Eindeutiger Index oder Primärschlüssel verletzt: "PUBLIC.CONSTRAINT_INDEX_4 ON PUBLIC.USERS(EMAIL NULLS FIRST) VALUES ( /* 1 */ 'email' )"
 Unique index or primary key violation: "PUBLIC.CONSTRAINT_INDEX_4 ON PUBLIC.USERS(EMAIL NULLS FIRST) VALUES ( /* 1 */ 'email' )"; SQL statement:
 INSERT INTO users(name, email, is_cool, bio, favorite_color, birthday, weight, balance) values ($1, $2, $3, $4, $5, $6, $7, $8) [23505-206]"""
-                val result =
-                    ExceptionInspector(Table("user"), User::class)
-                        .r2dbcDataIntegrityViolationException(
-                            R2dbcDataIntegrityViolationException(message),
-                            user
-                        )
-                expectThat(result).isA<UniqueConstraintViolatedException>().and {
-                    get { field }.isEqualTo(User::email)
-                    get { fieldValue }.isEqualTo("email-value")
-                }
+            val result =
+                ExceptionInspector(Table("user"), User::class)
+                    .r2dbcDataIntegrityViolationException(
+                        R2dbcDataIntegrityViolationException(message),
+                        user
+                    )
+            expectThat(result).isA<UniqueConstraintViolatedException>().and {
+                get { field }.isEqualTo(User::email)
+                get { fieldValue }.isEqualTo("email-value")
             }
         }
+    }
 }
