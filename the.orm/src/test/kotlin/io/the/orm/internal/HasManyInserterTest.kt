e62ff7b10ac155bc3b9c6ddc96cb01ffd30b6c51
Christoph Sturm <me@christophsturm.com>
2024-01-08 20:01:37 +0100

    just formatting

--- the.orm/src/test/kotlin/io/the/orm/internal/HasManyInserterTest.kt
+++ the.orm/src/test/kotlin/io/the/orm/internal/HasManyInserterTest.kt
@@ -4,7 +4,7 @@ import failgood.Test
 import failgood.mock.call
 import failgood.mock.getCalls
 import failgood.mock.mock
-import failgood.testsAbout
+import failgood.tests
 import io.the.orm.PKType
 import io.the.orm.Repo
 import io.the.orm.dbio.ConnectionProvider
@@ -25,47 +25,40 @@ object HasManyInserterTest {
 
     data class Entity(val name: String, val belongings: HasMany<Belonging>, val id: PKType? = null)
 
-    val context =
-        testsAbout(HasManyInserter::class) {
-            val connection = mock<ConnectionProvider>()
-            val belonging = Belonging("belonging 1")
-            val e = Entity("entity-name", hasMany(belonging))
-            val entity = EntityWrapper(e)
-            val entityWithId = EntityWrapper(e.copy(id = 42))
+    val context = tests {
+        val connection = mock<ConnectionProvider>()
+        val belonging = Belonging("belonging 1")
+        val e = Entity("entity-name", hasMany(belonging))
+        val entity = EntityWrapper(e)
+        val entityWithId = EntityWrapper(e.copy(id = 42))
 
-            val rootSimpleInserter =
-                mock<Inserter<Entity>> {
-                    method { create(connection, entity) }.returns(entityWithId)
-                }
-            val belongingRepo = mock<Repo<Belonging>>()
-            val subject =
-                HasManyInserter(
-                    rootSimpleInserter,
-                    ClassInfo(Entity::class, setOf(Belonging::class)),
-                    listOf(belongingRepo),
-                    listOf(
-                        ClassInfo(Belonging::class, setOf(Entity::class))
-                            .belongsToRelations
-                            .single()
-                    )
+        val rootSimpleInserter =
+            mock<Inserter<Entity>> { method { create(connection, entity) }.returns(entityWithId) }
+        val belongingRepo = mock<Repo<Belonging>>()
+        val subject =
+            HasManyInserter(
+                rootSimpleInserter,
+                ClassInfo(Entity::class, setOf(Belonging::class)),
+                listOf(belongingRepo),
+                listOf(
+                    ClassInfo(Belonging::class, setOf(Entity::class)).belongsToRelations.single()
                 )
-            it("inserts the created object") {
-                assert(subject.create(connection, entity) == entityWithId)
-                assert(
-                    getCalls(rootSimpleInserter).singleOrNull() ==
-                        call(Inserter<Entity>::create, connection, entity)
-                )
-            }
-            it("inserts the has many relations of the created entity") {
-                subject.create(connection, entity)
-                val belongingWithId =
-                    belonging.copy(
-                        entity = BelongsTo.AutoGetFromHasMany<Entity>().apply { id = 42 }
-                    )
-                assertEquals(
-                    call(Repo<Belonging>::create, connection, belongingWithId),
-                    getCalls(belongingRepo).singleOrNull()
-                )
-            }
+            )
+        it("inserts the created object") {
+            assert(subject.create(connection, entity) == entityWithId)
+            assert(
+                getCalls(rootSimpleInserter).singleOrNull() ==
+                    call(Inserter<Entity>::create, connection, entity)
+            )
         }
+        it("inserts the has many relations of the created entity") {
+            subject.create(connection, entity)
+            val belongingWithId =
+                belonging.copy(entity = BelongsTo.AutoGetFromHasMany<Entity>().apply { id = 42 })
+            assertEquals(
+                call(Repo<Belonging>::create, connection, belongingWithId),
+                getCalls(belongingRepo).singleOrNull()
+            )
+        }
+    }
 }
