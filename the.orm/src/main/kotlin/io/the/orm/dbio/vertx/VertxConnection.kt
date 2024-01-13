fa306cc36716b5642cd0461531b517dbf0cea64d
Christoph Sturm <me@christophsturm.com>
2024-01-13 22:54:16 +0100

    simplify

--- the.orm/src/main/kotlin/io/the/orm/dbio/vertx/VertxConnection.kt
+++ the.orm/src/main/kotlin/io/the/orm/dbio/vertx/VertxConnection.kt
@@ -7,17 +7,14 @@ import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.sqlclient.SqlConnection
 
 class VertxConnection(private val client: SqlConnection) : DBConnection {
-    override fun createStatement(sql: String): Statement {
-        return VertxStatement(client.preparedQuery(sql), sql)
-    }
+    override fun createStatement(sql: String): Statement =
+        VertxStatement(client.preparedQuery(sql), sql)
 
-    override fun createInsertStatement(sql: String): Statement {
-        return createStatement("$sql returning id")
-    }
+    override fun createInsertStatement(sql: String): Statement =
+        createStatement("$sql returning id")
 
-    override suspend fun beginTransaction(): DBTransaction {
-        return VertxTransaction(client.begin().coAwait())
-    }
+    override suspend fun beginTransaction(): DBTransaction =
+        VertxTransaction(client.begin().coAwait())
 
     override suspend fun close() {
         client.close().coAwait()
